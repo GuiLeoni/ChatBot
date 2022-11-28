63481f1e8d93d324f9e77ea7f7fa2ca997274350
Brambila08 <93653130+Brambila08@users.noreply.github.com>
2022-11-28 11:23:49 -0300

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,4 +1,6 @@
 from cgitb import text
+from email import message
+from tkinter import messagebox
 from turtle import width
 from selenium import webdriver
 from tkinter import *
@@ -6,7 +8,9 @@ from tkinter import filedialog
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from tkinter import messagebox
 import time
+
 
 
 # Contatos/Grupos - Informar o nomes de Grupos ou Contatos
@@ -20,15 +24,31 @@ mensagem = []
 # Midia = imagem, pdf, documentos e videos
 midia = []
 
+def validarcontato():
+    if len (inputcontato.get()) != 0 :
+        selecionarcontato()
+    else : 
+        messagebox.showerror("Error","digite um contato")
+        return ()
+          
 
 def selecionarcontato():
     contatos.append(inputcontato.get())
     print(contatos)
     inputcontato.delete(0,"end")
+    listarcontatos["text"] = ', '.join(contatos)
     
 def removercontato():
     contatos.clear()
     print(contatos) 
+    listarcontatos ["text"] = ""
+    
+def validarmensagen():
+    if len (inputmensagem.get("1.0","end-1c")) != 0 :
+        selecionarmensagem()
+    else : 
+        messagebox.showerror("Error","digite um mensagem")
+        return ()    
 
 def selecionarmensagem():
     mensagem.append(inputmensagem.get("1.0","end-1c"))
@@ -36,8 +56,9 @@ def selecionarmensagem():
     
 def removermensagem():
     mensagem.clear()
-    print(mensagem)   
+    print(mensagem)  
     
+  
 def selecionarimagem():
     imagem=filedialog.askopenfilename(initialdir="/",title="imagem",filetypes=(("imgfiles",["png","jpeg","jpg"]),("all files","*.*")))
     midia.append(imagem)    
@@ -45,7 +66,34 @@ def selecionarimagem():
     
 def removermidia():
     midia.clear()
-    print(midia)      
+    print(midia)    
+    
+def center(app):
+    
+    app.update_idletasks()
+    width = app.winfo_width()
+    frm_width = app.winfo_rootx() - app.winfo_x()
+    win_width = width + 3 * frm_width
+    height = app.winfo_height()
+    titlebar_height = app.winfo_rooty() - app.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = app.winfo_screenwidth() // 3 - win_width // 3
+    y = app.winfo_screenheight() // 3 - win_height // 3
+    app.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    app.deiconify()
+    
+    
+def validarenvio():
+    if len (contatos) != 0 :
+        if  len (inputmensagem.get("1.0","end-1c"))!=0 :
+            executarbot()
+        elif len (midia)!=0 :
+            executarbot()
+        else : 
+            messagebox.showerror("Error","digite uma mensagem")
+    else : 
+         messagebox.showerror("Error","digite um contato")     
+
 
 
 def executarbot():
@@ -97,6 +145,7 @@ def executarbot():
 
 app = Tk()
 app.title = "botwhats"
+center(app)
 app.geometry("520x470")
 
 
@@ -104,15 +153,18 @@ Label(app,text="ChatBot").place(x=10,y=20)
 Label(app,text="Digite seu contato:").place(x=10,y=70)
 inputcontato = Entry(app)
 inputcontato.place(x=120,y=70)
-Button(app,text="Adicionar contato", command=selecionarcontato).place(x=250,y=65)
+Button(app,text="Adicionar contato", command=validarcontato).place(x=250,y=65)
 Button(app,text="Remover contato", command=removercontato).place(x=360,y=65)
 Label(app,text="Digite sua mensagem:").place(x=10,y=130)
 inputmensagem = Text(app)
 inputmensagem.place(x=10,y=160,width=500,height=250)
-Button(app,text="Adicionar mensagem",command=selecionarmensagem).place(x=135,y=130)
+Button(app,text="Adicionar mensagem",command=validarmensagen).place(x=135,y=130)
 Button(app,text="Remover mensagem", command=removermensagem).place(x=265,y=130)
 Button(app,text="Adicione sua mídia", command=selecionarimagem).place(x=10,y=420)
 Button(app,text="Remover mídia", command=removermidia).place(x=130,y=420)
-Button(app,text="Enviar", command=executarbot).place(x=465,y=420)
-
+Button(app,text="Enviar", command=validarenvio).place(x=465,y=420)
+listarcontatos = Label (app,text="")
+listarcontatos.place (x=10,y=90)
+exitpagina = Button (app,text="Exit", command=app.destroy)
+exitpagina.place (x=425, y=420)
 app.mainloop()
